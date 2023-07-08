7d04eabb85acf0d4257669c8d43c5a79403eb0fc
Aaron Flores <aaron_flores08@yahoo.com>
2023-07-07 23:24:00 -0500

    set up readme, html, and js, added api link

---
+++ script.js
@@ -0,0 +1,8 @@
+
+// You will use localStorage to store any persistent data. For more information on how to work with the OpenWeather
+
+// Calling the api
+api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+
+https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
