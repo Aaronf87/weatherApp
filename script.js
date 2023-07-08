54e683b6a289bc1a37a89b162bf80a4a39081993
Aaron Flores <aaron_flores08@yahoo.com>
2023-07-08 15:55:12 -0500

    added style.css with background and styling

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
 // You will use localStorage to store any persistent data. For more information on how to work with the OpenWeather
+// using jquery?
 
 // Calling the api
 api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 
  `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={452cbddcfb906f1c82bbd2b57e972548}`
+
+ 
