7228b5cd7a97ca33b345a9f1fc9857dfdb6410e7
Aaron Flores <aaron_flores08@yahoo.com>
2023-07-09 12:26:00 -0500

    added variables to js script

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+var lat
+var lon 
+var n = $("new");
+
+console.log("hi");
+
+    $("#submit").click(function(){
+      $("#results").text(n);
+      console.log(n);
+    });
+
 
 // You will use localStorage to store any persistent data. For more information on how to work with the OpenWeather
-// using jquery?
 
 // Calling the api
-api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={452cbddcfb906f1c82bbd2b57e972548}
 
 
- `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={452cbddcfb906f1c82bbd2b57e972548}`
+// `https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={}`
 
- 
+
