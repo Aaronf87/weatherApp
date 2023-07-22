36cb0deb4d7b731135f89f95579af17e6c6c0dd3
Aaron Flores <aaron_flores08@yahoo.com>
2023-07-21 21:18:25 -0600

    added css and added fetch url

--- script.js
+++ script.js
@@ -6,45 +6,40 @@ function time(){
   var date = document.getElementById("time")
   // rendering date to id of time using day js
   date.textContent = myDate;
-}setInterval(time, 1000);
+}
+setInterval(time, 1000);
 
-
+function getGetWeatherApi(){
 // my API key---------------------------------------------------------------
+var apiKey = "452cbddcfb906f1c82bbd2b57e972548";
 // var lat 
 // var lon 
-var apiKey = "452cbddcfb906f1c82bbd2b57e972548";
 // getting the url from weather api---------------------------------------
 
 var cityName = document.getElementById("city").value;
-var r = document.getElementById("results");
-r.innerContent = cityName;
-// var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + apiKey;
-// // "api.openweathermap.org/data/2.5/forecast?lat="+ lat+ "&lon="+lon+"&appid=" + apiKey
+var result = document.getElementById("results");
+
+var requestUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + apiKey;
+// "api.openweathermap.org/data/2.5/forecast?lat="+ lat+ "&lon="+lon+"&appid=" + apiKey
 
 // document.getElementById("submit").addEventListener("click", subClick);
 
 // function subClick(cityName){
-//   console.log(cityName);
-//   getWeatherApi();
-// };
+  console.log(result);
+
 
 // // Using the weather api and fetchin data------------------------
-//     function getWeatherApi(){
-    
-//       fetch(requestUrl)
-//       .then(function(response){
-//       return response.json();
-//       })
-//       .then(function(response){
-//       console.log(response);
-//       })
-//       }
+      fetch(requestUrl)
+      .then(function(response){
+      return response.json();
+      })
+      .then(function(response){
+      console.log(response);
+      })
+      }
   
-
-
 // You will use localStorage to store any persistent data. For more information on how to work with the OpenWeather
 
-// Calling the api
-// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={452cbddcfb906f1c82bbd2b57e972548}
+ // api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={452cbddcfb906f1c82bbd2b57e972548}
 
-
+getGetWeatherApi();
